1957b9afa4b9cf1bc914a42590298e50f394343c
amscu-dev <amarinescu.dev@gmail.com>
2025-12-01 22:54:15 +0200

    exported night greet

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { nightGreet } from "./nightGreet";
 export { eveningGreet } from "./eveningGreet";
 export { morningGreet } from "./morningGreet";
 export { afternoonGreet } from "./afternoonGreet";
